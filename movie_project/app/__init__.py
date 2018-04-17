859e052ba1fb514866c4fa92d674be2d9820762c
fjkang <fjkang1991@gmail.com>
2018-04-17 16:21:23 +0800

    规范文件结构

--- movie_project/app/__init__.py
+++ movie_project/app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.debug = True
+
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+login = LoginManager()
+login.init_app(app)
+login.login_view = 'admin.login'
 
 from app.admin import admin as admin_blueprint
 from app.home import home as home_blueprint
---
+++ movie_project/config.py
@@ -0,0 +1,6 @@
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/movie?charset=utf8"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECRET_KEY = 'A LONG LONG LONG KEY'
+
+DEBUG = True
